3e2cdb7e873202515bf17698eef993de3075686f
Ruan Alberts <ruan.alberts@outlook.com>
2023-02-08 21:12:13 +0200

    created calc display

--- smart_calc.py
+++ smart_calc.py
@@ -1,9 +1,18 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication, 
+    QMainWindow, 
+    QWidget,
+    QLineEdit,
+    QPushButton,
+    QVBoxLayout
+)
 
 HWINDOW_SIZE = 80
 VWINDOW_SIZE = 250
+DISPLAY_HEIGHT = 20
 
 class calcWindow(QMainWindow):
     # the calc's main window
@@ -12,8 +21,18 @@ class calcWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("smart_calc")
         self.setFixedSize(VWINDOW_SIZE, HWINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
         centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
+        self.createDisplay()
+        
+
+    def createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.generalLayout.addWidget(self.display)
 
 def main():
     calcApp = QApplication([])
